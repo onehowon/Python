dcc88c37a05e63bbc829ee8314561893e2e98a4d
OneHoWon <81984723+onehowon@users.noreply.github.com>
2021-05-03 21:29:37 +0900

    Update 파이썬 실습 1차.py

--- 강의/파이썬 실습 1차.py
+++ 강의/파이썬 실습 1차.py
@@ -87,3 +87,18 @@ else:
 #배송비를 출력한다.
 print("배송비 =", shipping_cost)
 
+##
+#   이 프로그램은 사용자의 입력을 검증한다.
+#
+print("=====================")
+print("메뉴 1번: 치즈 버거")
+print("메뉴 2번: 치킨 버거")
+print("메뉴 3번: 불고기 버거")
+print("=====================")
+
+selection = int(input("메뉴를 선택하세요:"))
+
+if selection >=1 and selection <= 3:
+    print("메뉴", selection)
+else:
+    print("잘못 입력하셨습니다.")
