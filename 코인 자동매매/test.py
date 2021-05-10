a7f6d483f59ab39b3e74be88a784968f2c18fc3d
OneHoWon <81984723+onehowon@users.noreply.github.com>
2021-05-10 11:21:06 +0900

    Update test.py

--- 코인 자동매매/test.py
+++ 코인 자동매매/test.py
@@ -1,7 +1,7 @@
 import pyupbit
 
-access = "TIomsZW1fMWmx7NMy0CoAqr30DD2gEaLE6eRhaiS"
-secret = "9atdEcuwxWIt7AXyOGJ88LeEEe9bItGFG7gFNOPa"
+access = "your-access"
+secret = "your-secret"
 upbit = pyupbit.Upbit(access, secret)
 
 print(upbit.get_balance("KRW-XRP"))
