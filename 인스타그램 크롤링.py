bbf8f370a4f4b81adfb526f27663ee4eada6ce36
onehowon <flash0221@naver.com>
2021-07-13 15:53:34 +0900

    insta

---
+++ 인스타그램 크롤링.py
@@ -0,0 +1,8 @@
+from selenium import webdriver as wd
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
+import re
+import json
+import pandas as pd
