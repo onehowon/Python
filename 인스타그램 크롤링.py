cdccbfa7cd94f397f3f39d94740eae1585a62dd7
onehowon <flash0221@naver.com>
2021-07-13 17:19:19 +0900

    source

--- 인스타그램 크롤링.py
+++ 인스타그램 크롤링.py
@@ -6,3 +6,32 @@ import time
 import re
 import json
 import pandas as pd
+
+insta_id = "01065778166", insta_pw = "fpdltm20010221!"
+login_option = "instagram" # instagram
+driver_path = "~/chromedriver"
+instagram_id_name = "one_ho_won"
+instagram_login_btn = ""
+
+print(f"login start - option {login_option}")
+
+login_url = "https://www.instagram.com/accounts/login/"
+driver.get(login_url)
+time.sleep(10)
+
+if login_option == "instagram":
+    try:
+        instagram_id_form = driver.find_element_by_name(instagram_id_name)
+        instagram_id_form.send_keys(user_id)
+        time.sleep(5)
+
+        instagram_pw_form = driver.find_element_by_name(instagram_pw_name)
+        instagram_pw_form.send_keys(user_passwd)
+        time.sleep(7)
+
+        login_ok_button = driver.find_element_by_css_selector(instagram_login_btn)
+        login_ok_button.click()
+        is_login_success = True
+    except:
+        print("instagram login failed")
+        is_login_success = False
